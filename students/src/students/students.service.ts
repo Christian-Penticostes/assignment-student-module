fa5c59eea1fc225a521e13e03bdadbe130c89759
Christian-Penticostes <pentichris0@gmail.com>
2025-01-11 00:51:40 +0800

    Create Students

---
+++ students/src/students/students.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { CreateStudentDto } from './dto/create-student.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Student } from './student.entity';
+
+@Injectable()
+export class StudentsService {
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+  ) {}
+
+  async create(createStudentDto: CreateStudentDto) {
+    return await this.studentRepository.save(createStudentDto);
+  }
+}
